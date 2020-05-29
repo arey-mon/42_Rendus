180ba8db20adbd8edd079ecfd30ab4dee3ba9113
arey-mon <reymond.apolline@gmail.com>
2020-05-29 17:46:01 +0200

    clean_server

--- Ft_server/data/srcs/start.sh
+++
@@ -1,3 +0,0 @@
-#PACKAGES
-#will be used for configuration files
-echo "I did something from the ground. Well... I just went into start.sh and printed that. Isn't it amazing ?"
--- Ft_server/data2.0/srcs/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "daemon off;" >> /etc/nginx/nginx.conf
-service mysql start
-service php7.3-fpm start
-service nginx start
-bash
